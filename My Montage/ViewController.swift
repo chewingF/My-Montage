fb06bcf8223d83ed64b73c8e31a1fb909887fa92
陈念 <HarryC@chenniandemacbook.local>
2017-05-08 21:51:51 +1000

    Initial Commit

---
+++ My Montage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My Montage
+//
+//  Created by 陈念 on 17/5/8.
+//  Copyright © 2017年 nche75. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
